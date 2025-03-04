6eda70406e0e2bb70806d627f0de5dabaa19c075
muzilicheng <49356756+muzilicheng@users.noreply.github.com>
2025-03-04 16:18:39 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@ class Config {
   constructor() {
     this.baseURL = 'https://nodepay.org';
     this.ipCheckURL = 'https://ipinfo.io/json';
-    this.pingURL = 'http://nw.nodepay.ai/api/network/ping';
+    this.pingURL = 'http://nw.nodepay.org/api/network/ping';
     this.retryInterval = 30000;
-    this.sessionURL = 'http://api.nodepay.ai/api/auth/session';
+    this.sessionURL = 'http://api.nodepay.org/api/auth/session';
   }
 }
 
